be94474419f94604972f669a8e2651ee886c2aa0
Igor Escobar <igor@joindrover.com>
2020-10-16 16:16:39 +0100

    Initialising the User struct with as empty strings instead of nil

--- lib/config_cat/user.ex
+++ lib/config_cat/user.ex
@@ -1,6 +1,6 @@
 defmodule ConfigCat.User do
   @enforce_keys :identifier
-  defstruct [:identifier, email: nil, country: nil, custom: nil]
+  defstruct [:identifier, email: "", country: "", custom: ""]
 
   def new(identifier, other_props \\ []) do
     %__MODULE__{identifier: identifier}
@@ -16,14 +16,15 @@ defmodule ConfigCat.User do
   defp do_get_attribute(user, "country"), do: user.country
   defp do_get_attribute(user, attribute), do: custom_attribute(user.custom, attribute)
 
-  defp custom_attribute(nil, _attribute), do: nil
+  defp custom_attribute(nil, _attribute), do: ""
+  defp custom_attribute("", _attribute), do: ""
 
   defp custom_attribute(custom, attribute) do
     case Enum.find(custom, fn {key, _value} ->
            normalize(key) == attribute
          end) do
       {_key, value} -> value
-      _ -> nil
+      _ -> ""
     end
   end
 
