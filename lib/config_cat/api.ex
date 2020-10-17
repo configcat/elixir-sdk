585e16d3f2d11b4828980fe9276bcaba40469890
Randy Coulman <rcoulman@influxdata.com>
2020-10-17 14:26:54 -0700

    fix: use version from mix.exs in user agent headers

--- lib/config_cat/api.ex
+++ lib/config_cat/api.ex
@@ -2,9 +2,12 @@ defmodule ConfigCat.API do
   use HTTPoison.Base
 
   def process_request_headers(headers) do
+    version = Application.spec(:config_cat, :vsn) |> to_string()
+    user_agent = "ConfigCat-Elixir/m-#{version}"
+
     [
-      {"User-Agent", "ConfigCat-Elixir/m-0.0.1"},
-      {"X-ConfigCat-UserAgent", "ConfigCat-Elixir/m-0.0.1"},
+      {"User-Agent", user_agent},
+      {"X-ConfigCat-UserAgent", user_agent},
       {"Content-Type", "application/json"} | headers
     ]
   end
