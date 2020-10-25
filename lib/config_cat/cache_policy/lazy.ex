89b94c697ac9946d5773f9a9a8b3a8d8fa467359
Randy Coulman <rcoulman@influxdata.com>
2020-10-24 23:54:36 -0700

    feat: implement fetch behavior for lazy cache policy

--- lib/config_cat/cache_policy/lazy.ex
+++ lib/config_cat/cache_policy/lazy.ex
@@ -23,9 +23,10 @@ defmodule ConfigCat.CachePolicy.Lazy do
 
     initial_state =
       options
-      |> Keyword.take([:cache_api, :cache_key])
+      |> Keyword.take([:cache_api, :cache_key, :fetcher_api, :fetcher_id])
       |> Enum.into(%{})
       |> Map.merge(policy_options)
+      |> Map.put(:last_update, nil)
 
     GenServer.start_link(__MODULE__, initial_state, name: name)
   end
@@ -41,15 +42,77 @@ defmodule ConfigCat.CachePolicy.Lazy do
   end
 
   @impl CachePolicy
-  def force_refresh(_policy_id) do
-    {:error, :not_implemented}
+  def force_refresh(policy_id) do
+    GenServer.call(policy_id, :force_refresh)
   end
 
   @impl GenServer
   def handle_call(:get, _from, state) do
+    with {:ok, new_state} <- maybe_refresh(state) do
+      cached_config(new_state)
+    end
+  end
+
+  @impl GenServer
+  def handle_call(:force_refresh, _from, state) do
+    case refresh_and_record(state) do
+      {:ok, new_state} ->
+        {:reply, :ok, new_state}
+
+      error ->
+        {:reply, error, state}
+    end
+  end
+
+  defp cached_config(state) do
     cache_api = Map.fetch!(state, :cache_api)
     cache_key = Map.fetch!(state, :cache_key)
 
     {:reply, cache_api.get(cache_key), state}
   end
+
+  defp maybe_refresh(state) do
+    if needs_fetch?(state) do
+      refresh_and_record(state)
+    else
+      {:ok, state}
+    end
+  end
+
+  defp needs_fetch?(%{last_update: nil}), do: true
+
+  defp needs_fetch?(%{cache_expiry_seconds: expiry_seconds, last_update: last_update}) do
+    :gt !==
+      last_update
+      |> DateTime.add(expiry_seconds, :second)
+      |> DateTime.compare(now())
+  end
+
+  defp refresh_and_record(state) do
+    case refresh(state) do
+      :ok -> {:ok, %{state | last_update: now()}}
+      error -> error
+    end
+  end
+
+  defp refresh(state) do
+    api = Map.fetch!(state, :fetcher_api)
+    fetcher_id = Map.fetch!(state, :fetcher_id)
+
+    case api.fetch(fetcher_id) do
+      {:ok, :unchanged} ->
+        :ok
+
+      {:ok, config} ->
+        cache = Map.fetch!(state, :cache_api)
+        cache_key = Map.fetch!(state, :cache_key)
+        cache.set(cache_key, config)
+        :ok
+
+      error ->
+        error
+    end
+  end
+
+  defp now, do: DateTime.utc_now()
 end
--- test/config_cat/cache_policy/lazy_test.exs
+++ test/config_cat/cache_policy/lazy_test.exs
@@ -3,13 +3,23 @@ defmodule ConfigCat.CachePolicy.LazyTest do
 
   import Mox
 
-  alias ConfigCat.CachePolicy
+  alias ConfigCat.{CachePolicy, MockCache, MockFetcher}
   alias ConfigCat.CachePolicy.Lazy
-  alias ConfigCat.MockCache
+  alias HTTPoison.Response
 
   @cache_key "CACHE_KEY"
+  @fetcher_id :fetcher_id
 
   setup :verify_on_exit!
+
+  setup do
+    config = %{"some" => "config"}
+
+    MockCache
+    |> stub(:get, fn @cache_key -> {:ok, config} end)
+
+    {:ok, config: config}
+  end
 
   describe "creation" do
     test "returns a struct with the expected polling mode and options" do
@@ -21,24 +31,110 @@ defmodule ConfigCat.CachePolicy.LazyTest do
   end
 
   describe "getting the config" do
-    test "returns the cached config" do
-      config = %{"some" => "config"}
-      policy_id = UUID.uuid4() |> String.to_atom()
+    test "fetches config when first requested", %{config: config} do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 300)
 
-      {:ok, _pid} =
-        CachePolicy.start_link(
-          cache_api: MockCache,
-          cache_key: @cache_key,
-          cache_policy: CachePolicy.lazy(cache_expiry_seconds: 60),
-          name: policy_id
-        )
-
-      allow(MockCache, self(), policy_id)
-
-      MockCache
-      |> stub(:get, fn @cache_key -> {:ok, config} end)
+      expect_refresh(config)
 
       assert {:ok, ^config} = Lazy.get(policy_id)
     end
+
+    test "doesn't re-fetch if cache has not expired", %{config: config} do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 300)
+
+      expect_refresh(config)
+
+      Lazy.force_refresh(policy_id)
+
+      MockFetcher
+      |> expect(:fetch, 0, fn @fetcher_id -> {:ok, :unchanged} end)
+
+      MockCache
+      |> expect(:set, 0, fn @cache_key, _config -> :ok end)
+
+      Lazy.get(policy_id)
+    end
+
+    test "re-fetches if cache has expired", %{config: config} do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 0)
+      old_config = %{"old" => "config"}
+
+      expect_refresh(old_config)
+      Lazy.force_refresh(policy_id)
+
+      expect_refresh(config)
+      assert {:ok, ^config} = Lazy.get(policy_id)
+    end
+  end
+
+  describe "refreshing the config" do
+    test "stores new config in the cache", %{config: config} do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 300)
+
+      expect_refresh(config)
+
+      assert :ok = Lazy.force_refresh(policy_id)
+    end
+
+    test "fetches new config even if cache is not expired", %{config: config} do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 300)
+
+      expect_refresh(config)
+      Lazy.force_refresh(policy_id)
+
+      expect_refresh(config)
+      Lazy.force_refresh(policy_id)
+    end
+
+    test "does not update config when server responds that the config hasn't changed" do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 300)
+
+      MockFetcher
+      |> stub(:fetch, fn @fetcher_id -> {:ok, :unchanged} end)
+
+      MockCache
+      |> expect(:set, 0, fn @cache_key, _config -> :ok end)
+
+      assert :ok = Lazy.force_refresh(policy_id)
+    end
+
+    @tag capture_log: true
+    test "handles error responses" do
+      {:ok, policy_id} = start_fetch_policy(cache_expiry_seconds: 300)
+
+      response = %Response{status_code: 503}
+
+      MockFetcher
+      |> stub(:fetch, fn @fetcher_id -> {:error, response} end)
+
+      assert {:error, ^response} = Lazy.force_refresh(policy_id)
+    end
+  end
+
+  defp start_fetch_policy(options) do
+    policy_id = UUID.uuid4() |> String.to_atom()
+
+    {:ok, _pid} =
+      CachePolicy.start_link(
+        cache_api: MockCache,
+        cache_key: @cache_key,
+        cache_policy: CachePolicy.lazy(options),
+        fetcher_api: MockFetcher,
+        fetcher_id: @fetcher_id,
+        name: policy_id
+      )
+
+    allow(MockCache, self(), policy_id)
+    allow(MockFetcher, self(), policy_id)
+
+    {:ok, policy_id}
+  end
+
+  defp expect_refresh(config) do
+    MockFetcher
+    |> stub(:fetch, fn @fetcher_id -> {:ok, config} end)
+
+    MockCache
+    |> expect(:set, fn @cache_key, ^config -> :ok end)
   end
 end
