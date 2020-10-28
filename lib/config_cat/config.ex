e246498fd14ec19d22af7c8cd3f3087751d6f386
Randy Coulman <rcoulman@influxdata.com>
2020-10-27 22:12:06 -0700

    types: remove stray todo
    Since we don't actually control the type of what a config looks like, we'll use the more generic `map()` type here instead.

--- lib/config_cat/config.ex
+++ lib/config_cat/config.ex
@@ -1,7 +1,6 @@
 defmodule ConfigCat.Config do
   @type comparator :: non_neg_integer()
   @type key :: String.t()
-  # TODO: flesh this out
   @type t :: map()
   @type value :: String.t() | boolean() | number()
   @type variation_id :: String.t()
