6de3c485e6c3d137d75a6c79327487af4441d231
Randy Coulman <rcoulman@influxdata.com>
2020-10-27 21:12:41 -0700

    refactor: default User.custom to empty map
    This makes typespecs cleaner by avoiding an unnecessary `nil`.
    
    Also removed an unnecessary `normalize` call in User (because attributes will always be strings) and then inlined `normalize` into the last place that used it.

--- lib/config_cat/user.ex
+++ lib/config_cat/user.ex
@@ -1,6 +1,6 @@
 defmodule ConfigCat.User do
   @enforce_keys :identifier
-  defstruct [:identifier, country: nil, email: nil, custom: nil]
+  defstruct [:identifier, country: nil, email: nil, custom: %{}]
 
   def new(identifier, other_props \\ []) do
     %__MODULE__{identifier: identifier}
@@ -8,7 +8,7 @@ defmodule ConfigCat.User do
   end
 
   def get_attribute(user, attribute) do
-    do_get_attribute(user, normalize(attribute))
+    do_get_attribute(user, attribute)
   end
 
   defp do_get_attribute(user, "Identifier"), do: user.identifier
@@ -16,18 +16,12 @@ defmodule ConfigCat.User do
   defp do_get_attribute(user, "Email"), do: user.email
   defp do_get_attribute(user, attribute), do: custom_attribute(user.custom, attribute)
 
-  defp custom_attribute(nil, _attribute), do: nil
-
   defp custom_attribute(custom, attribute) do
     case Enum.find(custom, fn {key, _value} ->
-           normalize(key) == attribute
+           to_string(key) == attribute
          end) do
       {_key, value} -> value
       _ -> nil
     end
-  end
-
-  defp normalize(attribute) do
-    to_string(attribute)
   end
 end
