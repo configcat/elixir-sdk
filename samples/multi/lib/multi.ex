710a3b7210aa806b012db7df438f6033ab26b623
Randy Coulman <rcoulman@influxdata.com>
2020-10-29 22:14:42 -0700

    feat: add a multi-instance example app
    This app starts two instances of ConfigCat with different SDK keys and different names, showing how we can interact with more than one instance of ConfigCat at a time.

---
+++ samples/multi/lib/multi.ex
@@ -0,0 +1,90 @@
+defmodule Multi do
+  @moduledoc """
+  A simple GenServer to run ConfigCat examples.
+
+  The ConfigCat GenServer is started by our application supervisor in `simple/application.ex`.
+
+  In the first ConfigCat config there is a 'keySampleText' setting with the following rules:
+  1. If the User's country is Hungary, the value should be 'Dog'
+  2. If the User's custom property - SubscriptionType - is unlimited, the value should be 'Lion'
+  3. In other cases there is a percentage rollout configured with 50% 'Falcon' and 50% 'Horse' rules.
+  4. There is also a default value configured: 'Cat'
+
+  In the second ConfigCat config there are `isPOCFeatureEnabled` and `isAwesomeFeatureEnabled` settings.
+  """
+
+  use GenServer, restart: :transient
+
+  alias ConfigCat.User
+
+  def start_link(_options) do
+    GenServer.start_link(__MODULE__, [])
+  end
+
+  @impl GenServer
+  def init(_initial_state) do
+    run_first_examples()
+    run_second_examples()
+
+    {:ok, %{}}
+  end
+
+  defp run_first_examples do
+    # 1. As the passed User's country is Hungary this will print 'Dog'
+    my_setting_value =
+      ConfigCat.get_value("keySampleText", "default value", User.new("key", country: "Hungary"),
+        client: :first
+      )
+
+    print("keySampleText", my_setting_value)
+
+    # 2. As the passed User's custom attribute - SubscriptionType - is unlimited this will print 'Lion'
+    my_setting_value =
+      ConfigCat.get_value(
+        "keySampleText",
+        "default value",
+        User.new("key", custom: %{"SubscriptionType" => "unlimited"}),
+        client: :first
+      )
+
+    print("keySampleText", my_setting_value)
+
+    # 3/a. As the passed User doesn't fill in any rules, this will serve 'Falcon' or 'Horse'.
+    my_setting_value =
+      ConfigCat.get_value("keySampleText", "default value", User.new("key"), client: :first)
+
+    print("keySampleText", my_setting_value)
+
+    # 3/b. As this is the same user from 3/a., this will print the same value as the previous one ('Falcon' or 'Horse')
+    my_setting_value =
+      ConfigCat.get_value("keySampleText", "default value", User.new("key"), client: :first)
+
+    print("keySampleText", my_setting_value)
+
+    # 4. As we don't pass an User object to this call, this will print the setting's default value - 'Cat'
+    my_setting_value = ConfigCat.get_value("keySampleText", "default value", client: :first)
+    print("keySampleText", my_setting_value)
+
+    # 'myKeyNotExists' setting doesn't exist in the project configuration and the client returns default value ('default value')
+    my_setting_value = ConfigCat.get_value("myKeyNotExists", "default value", client: :first)
+    print("myKeyNotExists", my_setting_value)
+  end
+
+  def run_second_examples do
+    user =
+      User.new("Some UserID",
+        email: "configcat@example.com",
+        custom: %{version: "1.0.0"}
+      )
+
+    value = ConfigCat.get_value("isPOCFeatureEnabled", "default value", user, client: :second)
+    print("isPOCFeatureEnabled", value)
+
+    value = ConfigCat.get_value("isAwesomeFeatureEnabled", "default value", client: :second)
+    print("isAwesomeFeatureEnabled", value)
+  end
+
+  defp print(key, value) do
+    IO.puts("'#{key}' value from ConfigCat: #{value}")
+  end
+end
