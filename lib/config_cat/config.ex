97a66662f4ee145eb42f8d078297ab3f3c64c3d6
Randy Coulman <rcoulman@influxdata.com>
2020-10-31 15:36:31 -0700

    doc: add documentation to Config

--- lib/config_cat/config.ex
+++ lib/config_cat/config.ex
@@ -1,8 +1,20 @@
 defmodule ConfigCat.Config do
+  @moduledoc """
+  Defines configuration-related types used in the rest of the library.
+  """
+
   @typedoc false
   @type comparator :: non_neg_integer()
+
+  @typedoc "The identifier of a configuration setting."
   @type key :: String.t()
+
+  @typedoc "A collection of configuration settings."
   @type t :: map()
+
+  @typedoc "The actual value of a configuration setting."
   @type value :: String.t() | boolean() | number()
+
+  @typedoc "The identifier of a variation being tested."
   @type variation_id :: String.t()
 end
