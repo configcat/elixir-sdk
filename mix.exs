7fc77ef1b8ff28d9f22b82f1138bf6bb9b17384c
Igor Escobar <igor@joindrover.com>
2020-11-03 15:09:31 +0000

    Description was too long

--- mix.exs
+++ mix.exs
@@ -69,7 +69,6 @@ defmodule ConfigCat.MixProject do
     ConfigCat SDK for Elixir.
 
     Feature Flags created by developers for developers with ❤️. ConfigCat lets you manage feature flags across frontend, backend, mobile, and desktop apps without (re)deploying code. % rollouts, user targeting, segmentation. Feature toggle SDKs for all main languages.
-    Alternative to LaunchDarkly. Host yourself, or use the hosted management app at https://configcat.com.
     """
   end
 end
