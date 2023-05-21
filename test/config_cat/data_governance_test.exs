f652dc0a00f4ee5b58b1285259b7edbee6344217
Randy Coulman <rcoulman@gmail.com>
2023-05-20 22:49:37 -0700

    🔧 Enable credo strict mode
    Fix the only new issue reported.

--- test/config_cat/data_governance_test.exs
+++ test/config_cat/data_governance_test.exs
@@ -4,8 +4,8 @@ defmodule ConfigCat.ConfigFetcher.DataGovernanceTest do
   import Mox
 
   alias ConfigCat.CacheControlConfigFetcher, as: ConfigFetcher
-  alias ConfigCat.{Constants, MockAPI}
   alias ConfigCat.ConfigFetcher.RedirectMode
+  alias ConfigCat.{Constants, MockAPI}
   alias HTTPoison.Response
 
   require ConfigCat.Constants
