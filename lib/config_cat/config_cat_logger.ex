9fc34b724fc547b777e7f8cecff1e73297ef5d24
Randy Coulman <rcoulman@gmail.com>
2023-08-19 18:01:39 -0700

    ✨ Include the instance_id in log messages
    Don't bother for `debug` messages, but do it for the rest.

--- lib/config_cat/config_cat_logger.ex
+++ lib/config_cat/config_cat_logger.ex
@@ -4,7 +4,7 @@ defmodule ConfigCat.ConfigCatLogger do
   @spec debug(String.t()) :: Macro.t()
   @spec debug(String.t(), keyword()) :: Macro.t()
   defmacro debug(message, metadata \\ []) do
-    quote bind_quoted: [message: message, metadata: metadata] do
+    quote bind_quoted: [message: message, metadata: metadata, module: __MODULE__] do
       require Logger
 
       Logger.debug(message, metadata)
@@ -14,11 +14,14 @@ defmodule ConfigCat.ConfigCatLogger do
   @spec error(String.t()) :: Macro.t()
   @spec error(String.t(), keyword()) :: Macro.t()
   defmacro error(message, metadata \\ []) do
-    quote bind_quoted: [message: message, metadata: metadata] do
+    quote bind_quoted: [message: message, metadata: metadata, module: __MODULE__] do
       require Logger
 
       instance_id = Logger.metadata() |> Keyword.get(:instance_id)
-      Logger.error(message, metadata)
+
+      message
+      |> module.format_message(instance_id)
+      |> Logger.error(metadata)
 
       if instance_id do
         ConfigCat.Hooks.invoke_on_error(instance_id, message)
@@ -29,20 +32,33 @@ defmodule ConfigCat.ConfigCatLogger do
   @spec info(String.t()) :: Macro.t()
   @spec info(String.t(), keyword()) :: Macro.t()
   defmacro info(message, metadata \\ []) do
-    quote bind_quoted: [message: message, metadata: metadata] do
+    quote bind_quoted: [message: message, metadata: metadata, module: __MODULE__] do
       require Logger
 
-      Logger.info(message, metadata)
+      instance_id = Logger.metadata() |> Keyword.get(:instance_id)
+
+      message
+      |> module.format_message(instance_id)
+      |> Logger.info(metadata)
     end
   end
 
   @spec warn(String.t()) :: Macro.t()
   @spec warn(String.t(), keyword()) :: Macro.t()
   defmacro warn(message, metadata \\ []) do
-    quote bind_quoted: [message: message, metadata: metadata] do
+    quote bind_quoted: [message: message, metadata: metadata, module: __MODULE__] do
       require Logger
 
-      Logger.warn(message, metadata)
+      instance_id = Logger.metadata() |> Keyword.get(:instance_id)
+
+      message
+      |> module.format_message(instance_id)
+      |> Logger.warn(metadata)
     end
   end
+
+  @doc false
+  @spec format_message(String.t(), ConfigCat.instance_id() | nil) :: String.t()
+  def format_message(message, nil), do: message
+  def format_message(message, instance_id), do: "[" <> to_string(instance_id) <> "] " <> message
 end
