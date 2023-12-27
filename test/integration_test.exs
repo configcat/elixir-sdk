25fcab40e1bdc4e4aa7a5940ebfca957140719b1
Randy Coulman <rcoulman@gmail.com>
2023-12-27 14:07:40 -0800

    🦺 Validate SDK key format

--- test/integration_test.exs
+++ test/integration_test.exs
@@ -7,30 +7,69 @@ defmodule ConfigCat.IntegrationTest do
   alias ConfigCat.Cache
   alias ConfigCat.CachePolicy
   alias ConfigCat.InMemoryCache
+  alias ConfigCat.LocalMapDataSource
 
   @sdk_key "PKDVCLf-Hq-h-kCzMp-L7Q/PaDVCFk9EpmD6sLpGLltTA"
 
-  test "raises error if SDK key is missing" do
-    nil
-    |> start()
-    |> assert_sdk_key_required()
-  end
+  describe "SDK key validation" do
+    test "raises error if SDK key is missing" do
+      nil
+      |> start()
+      |> assert_sdk_key_required()
+    end
 
-  test "raises error if SDK key is an empty string" do
-    ""
-    |> start()
-    |> assert_sdk_key_required()
-  end
+    test "raises error if SDK key is an empty string" do
+      ""
+      |> start()
+      |> assert_sdk_key_required()
+    end
 
-  @tag capture_log: true
-  test "raises error when starting another instance with the same SDK key" do
-    {:ok, _} = start(@sdk_key, name: :original)
+    for sdk_key <- [
+          "key",
+          "configcat-proxy/key",
+          "1234567890abcdefghijkl01234567890abcdefghijkl",
+          "configcat-sdk-2/1234567890abcdefghijkl/1234567890abcdefghijkl",
+          "configcat/1234567890abcdefghijkl/1234567890abcdefghijkl"
+        ] do
+      test "raises error if SDK is invalid with SDK key: #{sdk_key}" do
+        sdk_key = unquote(sdk_key)
 
-    assert {:error, {{:EXIT, {error, _stacktrace}}, _spec}} =
-             start(@sdk_key, name: :duplicate)
+        sdk_key |> start() |> assert_sdk_key_invalid(sdk_key)
+      end
+    end
 
-    assert %ArgumentError{message: message} = error
-    assert message =~ ~r/existing ConfigCat instance/
+    test "allows older format SDK keys" do
+      assert {:ok, _} = start("1234567890abcdefghijkl/1234567890abcdefghijkl")
+    end
+
+    test "allows newer format SDK keys" do
+      assert {:ok, _} = start("configcat-sdk-1/1234567890abcdefghijkl/1234567890abcdefghijkl")
+    end
+
+    test "allows proxy SDK keys if base_url is specified" do
+      assert {:ok, _} = start("configcat-proxy/key", base_url: "base_url")
+    end
+
+    test "does not allow non-proxy SDK keys even if base_url is specified" do
+      sdk_key = "not-configcat-proxy/key"
+      sdk_key |> start(base_url: "base_url") |> assert_sdk_key_invalid(sdk_key)
+    end
+
+    test "does not validate SDK key format in local-only mode" do
+      overrides = LocalMapDataSource.new(%{}, :local_only)
+      assert {:ok, _} = start("invalid-sdk-key-format", flag_overrides: overrides)
+    end
+
+    @tag capture_log: true
+    test "raises error when starting another instance with the same SDK key" do
+      {:ok, _} = start(@sdk_key, name: :original)
+
+      assert {:error, {{:EXIT, {error, _stacktrace}}, _spec}} =
+               start(@sdk_key, name: :duplicate)
+
+      assert %ArgumentError{message: message} = error
+      assert message =~ ~r/existing ConfigCat instance/
+    end
   end
 
   test "fetches config" do
@@ -85,7 +124,7 @@ defmodule ConfigCat.IntegrationTest do
 
   @tag capture_log: true
   test "handles errors from ConfigCat server" do
-    {:ok, client} = start("invalid_sdk_key")
+    {:ok, client} = start("configcat-sdk-1/1234567890abcdefghijkl/1234567890abcdefghijkl")
 
     assert {:error, _message} = ConfigCat.force_refresh(client: client)
   end
@@ -120,6 +159,13 @@ defmodule ConfigCat.IntegrationTest do
     |> InMemoryCache.clear()
 
     start_config_cat(sdk_key, options)
+  end
+
+  defp assert_sdk_key_invalid({:error, result}, sdk_key) do
+    assert {{:EXIT, {error, _stacktrace}}, _spec} = result
+
+    expected_message = "SDK Key `#{sdk_key}` is invalid."
+    assert %ArgumentError{message: ^expected_message} = error
   end
 
   defp assert_sdk_key_required({:error, result}) do
