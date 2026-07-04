6cc4e14dc39ea69d69eaa141154dec0f812c0adb
Randy Coulman <randy@randycoulman.com>
2026-07-03 17:02:28 -0700

    Use less-strict patterns in dialyzer ignore
    Some of the message wording has changed from version to version.

--- .dialyzer_ignore.exs
+++ .dialyzer_ignore.exs
@@ -2,9 +2,8 @@
   # The following four ignore rules are due to an upstream type problem in
   # HTTPoison 3.0. Once https://github.com/edgurgel/httpoison/pull/511 has been
   # merged and released, we should be able to delete these rules.
-  {"deps/httpoison/lib/httpoison/base.ex", "Type mismatch with behaviour callback to stream_next/1."},
-  {"deps/httpoison/lib/httpoison/base.ex", "Type mismatch for @callback stream_next."},
-  {"deps/httpoison/lib/httpoison/base.ex",
-   "The pattern variable _3 can never match the type, because it is covered by previous clauses."},
-  {"lib/config_cat/api.ex", "Invalid type specification for function stream_next."}
+  {"deps/httpoison/lib/httpoison/base.ex", :callback_arg_type_mismatch},
+  {"deps/httpoison/lib/httpoison/base.ex", :callback_type_mismatch},
+  {"deps/httpoison/lib/httpoison/base.ex", :pattern_match_cov},
+  {"lib/config_cat/api.ex", :invalid_contract}
 ]
